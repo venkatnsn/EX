b954c70803ab794c07e153fd6628de90f764dc0c
Venkat <58161293+venkatnsn@users.noreply.github.com>
2021-08-18 10:26:35 +0530

    Add files via upload

---
+++ Exam.R
@@ -0,0 +1,17 @@
+data = matrix(0,10,2)
+View(data)
+
+colnames(data) = c('col1','col2')
+View(data)
+
+
+##### Section1#####
+
+variable = 100
+
+##### Section2#####
+
+variable = 100
+
+x = 26:35
+ 
